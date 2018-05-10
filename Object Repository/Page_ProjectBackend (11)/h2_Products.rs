60753dfccb94928a6b05660abbe25edef0cf5889
Chamnol <chamnol_yin@cmu.ac.th>
2018-05-11 00:54:44 +0700

    test case#11

---
+++ Object Repository/Page_ProjectBackend (11)/h2_Products.rs
@@ -0,0 +1,30 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>h2_Products</name>
+   <tag></tag>
+   <elementGuidId>319a0420-e204-450d-88d4-c0c02d4d667b</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>h2</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Products</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;add-row&quot;)/h2[1]</value>
+   </webElementProperties>
+</WebElementEntity>
