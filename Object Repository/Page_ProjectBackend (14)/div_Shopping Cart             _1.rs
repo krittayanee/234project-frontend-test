4b6ac7dd36dec92a004a003a4d31257b670094d3
Chamnol <chamnol_yin@cmu.ac.th>
2018-05-11 00:57:34 +0700

    test case#14

---
+++ Object Repository/Page_ProjectBackend (14)/div_Shopping Cart             _1.rs
@@ -0,0 +1,86 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_Shopping Cart             _1</name>
+   <tag></tag>
+   <elementGuidId>e4e47545-c355-4953-bfcc-3952b3c7e55b</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>id</name>
+      <type>Main</type>
+      <value>add-row</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+  
+    
+      Shopping Cart
+      
+      
+    
+    
+      
+        
+          #
+          Product Name
+          Price
+          amount
+        
+      
+      
+        
+          1
+          Garden
+          20,000 THB 
+          
+            
+              
+            
+          
+        
+          2
+          Banana
+          150 THB 
+          
+            
+              
+            
+          
+        
+      
+    
+    
+      
+        Total price:  20,150 THB
+      
+    
+    
+        
+             confirm
+        
+      
+  
+</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;add-row&quot;)</value>
+   </webElementProperties>
+</WebElementEntity>
---
+++ Object Repository/Page_ProjectBackend (14)/div_Shopping Cart.rs
@@ -0,0 +1,84 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_Shopping Cart</name>
+   <tag></tag>
+   <elementGuidId>858eea53-edeb-4a00-b5b8-78054c102742</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>col-12 col-md-9 col-xl-8 py-md-3 pl-md-5 bd-content</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+    
+      Shopping Cart
+      
+      
+    
+    
+      
+        
+          #
+          Product Name
+          Price
+          amount
+        
+      
+      
+        
+          1
+          Garden
+          20,000 THB 
+          
+            
+              
+            
+          
+        
+          2
+          Banana
+          150 THB 
+          
+            
+              
+            
+          
+        
+      
+    
+    
+      
+        Total price:  20,150 THB
+      
+    
+    
+        
+             confirm
+        
+      
+  </value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;add-row&quot;)/div[@class=&quot;col-12 col-md-9 col-xl-8 py-md-3 pl-md-5 bd-content&quot;]</value>
+   </webElementProperties>
+</WebElementEntity>
