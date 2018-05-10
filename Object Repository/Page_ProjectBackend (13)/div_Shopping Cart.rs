2e2077c76410253588dbb271aebe1eaa00c061d5
Chamnol <chamnol_yin@cmu.ac.th>
2018-05-11 00:56:43 +0700

    test case#13

---
+++ Object Repository/Page_ProjectBackend (13)/div_Shopping Cart.rs
@@ -0,0 +1,75 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>div_Shopping Cart</name>
+   <tag></tag>
+   <elementGuidId>c45ebdc6-693d-4ce1-a743-9054544ede8a</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>div</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>col-12 col-md-9 col-xl-8 py-md-3 pl-md-5 bd-content</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>
+    
+      Shopping Cart
+      
+      
+    
+    
+      
+        
+          #
+          Product Name
+          Price
+          amount
+        
+      
+      
+        
+          1
+          Garden
+          20,000 THB 
+          
+            
+              
+            
+          
+        
+      
+    
+    
+      
+        Total price:  20,000 THB
+      
+    
+    
+        
+             confirm
+        
+      
+  </value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;add-row&quot;)/div[@class=&quot;col-12 col-md-9 col-xl-8 py-md-3 pl-md-5 bd-content&quot;]</value>
+   </webElementProperties>
+</WebElementEntity>
---
+++ Object Repository/Page_ProjectBackend (13)/h2_Products.rs
@@ -0,0 +1,30 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>h2_Products</name>
+   <tag></tag>
+   <elementGuidId>93ddce98-fa86-4e3e-81d8-24582d74833f</elementGuidId>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>h2</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>text</name>
+      <type>Main</type>
+      <value>Products</value>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;add-row&quot;)/h2[1]</value>
+   </webElementProperties>
+</WebElementEntity>
